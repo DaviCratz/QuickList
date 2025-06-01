ee72e9c637a0e8a1348bf55ee1d1345f335a8034
davicratz <davicratz@gmail.com>
2025-06-01 20:30:57 +0000

    criação do main.js

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
-function main() {
-  console.log("Hello, World!");
-}
+const items = []
 
-main();
+function addItem() {
+  const itemName = document.querySelector("#item").value
+
+  const item = {
+    name: itemName,
+    checked: false,
+  }
+
+  items.push(item)
+
+  document.querySelector("#item").value = ""
+
+  console.log(items)
+}
